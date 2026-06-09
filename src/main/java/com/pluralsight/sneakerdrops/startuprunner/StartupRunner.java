fdcb6fa34d206740d347de8a57d86a7b611d8225
branRoberts <brandonkrob12@gmail.com>
2026-06-09 10:33:54 -0400

    Second Commit Copied my previous code into this one and added code to configure the connection to the database

--- src/main/java/com/pluralsight/sneakerdrops/startuprunner/StartupRunner.java
+++ src/main/java/com/pluralsight/sneakerdrops/startuprunner/StartupRunner.java
@@ -1,4 +1,22 @@
 package com.pluralsight.sneakerdrops.startuprunner;
 
-public class StartupRunner {
+import com.pluralsight.sneakerdrops.service.DropService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupRunner implements CommandLineRunner {
+
+    private DropService dropService;
+
+    @Autowired
+    public StartupRunner(DropService dropService) {
+        this.dropService = dropService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(dropService.getStatus());
+    }
 }
